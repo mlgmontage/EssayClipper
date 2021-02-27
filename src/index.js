5afe982b6f57c581869994fe2b72b964efce85e5
mlgmontage <kettleofrassel1@gmail.com>
2021-02-27 13:16:35 +0500

    setup bootstrap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
 ReactDOM.render(<h1>hello, world</h1>, document.getElementById("root"));
