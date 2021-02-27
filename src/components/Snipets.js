e4f812b5c26aaba8dad64de7c39bf096e4cd9f7c
mlgmontage <kettleofrassel1@gmail.com>
2021-02-27 21:32:15 +0500

    setup marked & display & rendering text

--- src/components/Snipets.js
+++ src/components/Snipets.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import marked from "marked";
+import data from "../data";
 
 /**
  * TODO: Next, prev buttons. Keybindings
@@ -6,8 +8,24 @@ import React, { Component } from "react";
  */
 
 class Snipets extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data,
+      index: 0,
+    };
+  }
   render() {
-    return <h1>Snipets</h1>;
+    return (
+      <div>
+        <h4>{this.state.data[this.state.index].title}</h4>
+        <div
+          dangerouslySetInnerHTML={{
+            __html: marked(this.state.data[this.state.index].markdown),
+          }}
+        ></div>
+      </div>
+    );
   }
 }
 
