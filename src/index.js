490c58117e086416750d32b18959568d1c9c8cc3
mlgmontage <kettleofrassel1@gmail.com>
2021-02-27 14:22:35 +0500

    app component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
+import App from "./App";
 
-ReactDOM.render(<h1>hello, world</h1>, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 module.hot.accept();
--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
   },
   devServer: {
     contentBase: path.join(__dirname, "dist"),
+    historyApiFallback: true,
   },
   plugins: [new webpack.HotModuleReplacementPlugin()],
   module: {
