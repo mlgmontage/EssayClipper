8cbd96c3f205a066d902d0c75a9759fce1e2a206
mlgmontage <kettleofrassel1@gmail.com>
2021-02-27 22:55:37 +0500

    create form

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 /**
- * TODO: Submit from & submit button
+ * TODO: Submit from & submit button & toggle button
  */
 
 class Create extends Component {
@@ -12,7 +12,25 @@ class Create extends Component {
   render() {
     return (
       <div>
-        <h4>Create</h4>
+        <div>
+          <div className="mt-3">
+            <input type="text" className="form-control" placeholder="title" />
+          </div>
+          <div className="mt-3">
+            <textarea
+              type="text"
+              className="form-control"
+              placeholder="markdown"
+              rows="20"
+            ></textarea>
+          </div>
+        </div>
+        <div
+          style={{ fontSize: "50px", cursor: "pointer" }}
+          className="text-center"
+        >
+          <i className="fa fa-paperclip"></i>
+        </div>
       </div>
     );
   }
