daa64f4f684ed36ad5c5f03595e619e515600d5e
mlgmontage <kettleofrassel1@gmail.com>
2021-02-27 20:12:42 +0500

    data modelling

---
+++ src/data.js
@@ -0,0 +1,5 @@
+export default [
+  { markdown: "## Hello", isArchived: false, comments: [{ markdown: "" }] },
+  { markdown: "### Second", isArchived: false, comments: [] },
+  { markdown: "**fat face**", isArchived: false, comments: [] },
+];
