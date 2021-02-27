4958a57a3fc68fbb7b8c90b4c866622012b0d2f7
mlgmontage <kettleofrassel1@gmail.com>
2021-02-27 23:19:54 +0500

    toggleEditMode button/funtion

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,36 +1,53 @@
 import React, { Component } from "react";
 
 /**
- * TODO: Submit from & submit button & toggle button
+ * TODO: toggle button, handle change
  */
 
 class Create extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      EditMode: true,
+    };
+    this.toggleEditMode = this.toggleEditMode.bind(this);
+  }
+
+  toggleEditMode() {
+    this.setState({
+      EditMode: !this.state.EditMode,
+    });
   }
 
   render() {
     return (
       <div>
-        <div>
-          <div className="mt-3">
-            <input type="text" className="form-control" placeholder="title" />
-          </div>
-          <div className="mt-3">
-            <textarea
-              type="text"
-              className="form-control"
-              placeholder="markdown"
-              rows="20"
-            ></textarea>
-          </div>
+        <div className="btn btn-secondary" onClick={this.toggleEditMode}>
+          <i className="fa fa-eye"></i>
         </div>
-        <div
-          style={{ fontSize: "50px", cursor: "pointer" }}
-          className="text-center"
-        >
-          <i className="fa fa-paperclip"></i>
-        </div>
+        {this.state.EditMode ? (
+          <div>
+            <div className="mt-3">
+              <input type="text" className="form-control" placeholder="title" />
+            </div>
+            <div className="mt-3">
+              <textarea
+                type="text"
+                className="form-control"
+                placeholder="markdown"
+                rows="20"
+              ></textarea>
+            </div>
+            <div
+              style={{ fontSize: "50px", cursor: "pointer" }}
+              className="text-center"
+            >
+              <i className="fa fa-paperclip"></i>
+            </div>
+          </div>
+        ) : (
+          <div> toggle </div>
+        )}
       </div>
     );
   }
