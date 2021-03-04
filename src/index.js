84986f96588847ba7446d71f62f61ec1d52704d6
mlgmontage <kettleofrassel1@gmail.com>
2021-03-04 18:30:02 +0500

    switching to github theme

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import marked from "marked";
 import highlightJS from "highlight.js";
 import "font-awesome/css/font-awesome.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "highlight.js/styles/obsidian.css";
+import "highlight.js/styles/github.css";
 import "./index.css";
 import App from "./App";
 
